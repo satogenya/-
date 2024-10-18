e4e3c8205c35f1c77a0590e40cf2c0b918ead76f
satogenya <t236045@st.yamagata-u.ac.jp>
2024-10-18 19:31:59 +0900

    Create tasizan.c

---
+++ tasizan.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int k,z,s;
+       printf("1つ目の数字 を教えて");
+        scanf("%d",&k);
+        
+        printf("2つ目の数字 を教えて");
+         scanf("%d",&z);
+        
+        s = k + z;
+        
+        printf("二つの数字の合計は%d\n",s);
+
+    return 0;
+}
